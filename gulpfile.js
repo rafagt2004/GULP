f3326aff570bc161cc0d815cc67a9d88a3758b2b
rafael <rafagtsouza.jr@gmail.com>
2025-09-15 20:18:57 -0300

    Refactor code structure for improved readability and maintainability

--- gulpfile.js
+++ gulpfile.js
@@ -22,39 +22,11 @@ function compilaSass(){
     return gulp.src('./source/styles/main.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
-            outputStyle: 'compressed' //nested, expanded, compact, compressed
+            outputStyle: 'compressed'
         }))
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./build/styles'));
-        
 }
-
-// Tarefas públicas
-function funcaoPadrao(callback){
-    setTimeout(function(){
-        console.log('Executando arquivo gulp');
-        callback();
-    }, 2000);
-
-}
-
-function dizOi(callback){
-    console.log('Oi, Gulp!');
-    dizTchau();
-    callback();
-}
-
-// Tarefa privada
-function dizTchau(){
-    console.log('Tchau, Gulp!');
-}
-
-// 1) Pública: que pode ser acessada através do comando: npm run gulp NOME_DA_TAREFA 
-// 2) Privada: que só é acessível apenas por outra tarefa no Gulp file. 
-// Ao executar o comando npm run gulp iremos receber uma mensagem de erro, 
-// informando que a tarefa padrão (default) não foi encontrada, isso ocorre 
-// pois no arquivo gulpfile.js não existe uma função para o exports.default.
-
 
 exports.default = function(){
     gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
